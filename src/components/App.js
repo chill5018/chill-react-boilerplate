5f1febf2bd8510481853c8ca4cd812da387a9904
Colin Hill <cwh@colinwhill.com>
2018-05-13 20:50:19 +0200

    [add] root html, and core app component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Comment {
+  render() {
+    return (
+      <div>
+        <h1>Chill-start to React</h1>
+      </div>
+    )
+  }
+}
+
+export default App
