afcb537f545c64d583772836225fc99c08259ce9
Colin Hill <cwh@colinwhill.com>
2018-05-13 21:17:02 +0200

    [add] most basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+
+module.exports = {
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve('./dist')
+  }
+}
+
